eed41af0c731bf86973848317b498b0dbf9c3c54
jimmoffet <jimmoffet@gmail.com>
2017-07-18 13:29:22 -0400

    add README

---
+++ google-doc-url.js
@@ -0,0 +1,2 @@
+var googleDocURL = 'http://docs.google.com/spreadsheets/d/13zMI6Q8ghEgQx1lvT_V5cHbMSUvdqSiiRV6UQs5puds/pubhtml'
+var googleDocURL2 = 'http://docs.google.com/spreadsheets/d/14EjCjlEzbu-F6kBtpv63hVbhVD5odeg6X8L0w1hQCIo/pubhtml'
