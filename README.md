0b986741cfdcecc21014163448321676e3a8efe9
jimmoffet <jimmoffet@gmail.com>
2018-02-12 00:17:03 -0500

    google doc url update

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ![Alt text](screen.png?raw=true "Direct Action Organizing Map")
 
-Demo can be found [here](https://s3.us-east-2.amazonaws.com/resistancemap/maptest/index.html "Title").
+Demo can be found [here](https://www.resistancemappingproject.com "Title").
+
+To deploy, rename google-doc-url_FILL_ME_IN.js to google-doc-url.js, create copies of the sheets at the included urls, publish the sheets from the google sheets editor (file > publish), then replace them in google-doc-url.js
--- google-doc-url_FILL_ME_IN.js
+++ google-doc-url_FILL_ME_IN.js
@@ -1,4 +1,4 @@
 // Don't forget to click file > publish in google sheets and use the link provided in that dialog
 // The links below will not work
-var googleDocURL = ''
-var googleDocURL2 = ''
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRB65sMM0Bv1HFy8TUok23pZt8GYao42PY8xBYVT2yiAIBnTydIISuMDeE73jB7sdPWMVr4oICDyjne/pubhtml'
+var googleDocURL2 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTr-_lG5Fg8NL4ybBmR-VEXUaS0g_ueGN2bSXFwmXe329LCHAjswVMx6LsMTkJMn1tpBof03RzYGTTb/pubhtml'
