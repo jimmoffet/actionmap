a45ee029f4135d4afeb204dd7f45773d98bc2b25
jimmoffet <jimmoffet@gmail.com>
2018-02-11 22:58:39 -0500

    google doc url update

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,4 @@
+// Don't forget to click file > publish in google sheets and use the link provided in that dialog
+// The links below will not work
 var googleDocURL = 'http://docs.google.com/spreadsheets/d/13zMI6Q8ghEgQx1lvT_V5cHbMSUvdqSiiRV6UQs5puds/pubhtml'
 var googleDocURL2 = 'http://docs.google.com/spreadsheets/d/14EjCjlEzbu-F6kBtpv63hVbhVD5odeg6X8L0w1hQCIo/pubhtml'
---
+++ google-doc-url_FILL_ME_IN.js
@@ -0,0 +1,4 @@
+// Don't forget to click file > publish in google sheets and use the link provided in that dialog
+// The links below will not work
+var googleDocURL = ''
+var googleDocURL2 = ''
